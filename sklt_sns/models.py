8e2a72946c6926f3a89f47c67d8c50e4c587a9ca
Tranmai96 <baotran.mhoang@gmail.com>
2020-06-10 15:29:23 +0900

    First commit

--- sklt_sns/models.py
+++ sklt_sns/models.py
@@ -4,10 +4,12 @@ from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
+
 	name = models.CharField(max_length=200, null=True)
 	id_num = models.FloatField()
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
 
 	def __str__(self):
 		return self.name
@@ -17,7 +19,6 @@ class Post(models.Model):
     author = models.ForeignKey('User', on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
     like = models.ManyToManyField(User, related_name='like')
 
 
--- sklt_sns/views.py
+++ sklt_sns/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'sklt_sns/main.html')
+    return render(request, 'sklt_sns/home.html')
 
 def post_detail(request):
     return HttpResponse('Post detail')
