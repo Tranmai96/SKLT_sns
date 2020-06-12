1e5904f8c1b63ef9be9bfab219dcd9237be9069c
Your Name <you@example.com>
2020-06-12 16:55:24 +0900

    master

--- sklt_sns/urls.py
+++ sklt_sns/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.index, name='index'),
     path('post_detail',views.post_detail),
     path('profile',views.profile),
-    path('search_result',views.search_result),
     path('log_in_page',views.log_in_page),
 
 ]
- 
--- sklt_sns/views.py
+++ sklt_sns/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
-def home(request):
-    posts=Post.objects.order_by('-created_date')
+def index(request):
+    posts = Post.objects.order_by('-created_date')
+    keyword = request.GET.get('keyword')
     latest_posts = []
-    for i in range(6):
-        latest_posts.append(posts[i])
-
+    if keyword:
+        posts1 = posts.filter(
+                 Q(text__icontains=keyword) | Q(author__name__icontains=keyword)
+               )
+        for i in posts1:
+            latest_posts.append(i)
+        
+        messages.success(request, '「{}」の検索結果'.format(keyword))
+    else:
+        for i in range(6):
+            latest_posts.append(posts[i])
     context={'posts':posts,'latest_posts':latest_posts}
     return render(request, 'sklt_sns/home.html',context)
 
@@ -23,5 +34,6 @@ def search_result(request):
 
 def log_in_page(request):
     return HttpResponse('Log in page')
+
 
 
